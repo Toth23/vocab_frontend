51fe07b440148f2af53b7d43904d13a05d338f41
Toth23 <thiagor@safe-mail.net>
2023-11-14 22:35:12 +0100

    add tests for example display

---
+++ src/ExampleDisplay.test.tsx
@@ -0,0 +1,56 @@
+import {render, screen} from "@testing-library/react";
+import {ExampleDisplay} from "./ExampleDisplay.tsx";
+import userEvent from "@testing-library/user-event";
+
+
+describe('Example Display', () => {
+  const wordId = 123;
+  const examples = [{example: "ex 1", id: 1}, {example: "ex 2", id: 2}];
+  const deleteExample = jest.fn();
+
+  afterEach(() => {
+    deleteExample.mockReset();
+  });
+
+  it('should display examples', () => {
+    // when
+    render(<ExampleDisplay wordId={wordId} examples={examples} showExamples={true} deleteExample={deleteExample}/>);
+
+    // then
+    expect(screen.getByText(examples[0].example)).toBeInTheDocument();
+    expect(screen.getByText(examples[1].example)).toBeInTheDocument();
+  });
+
+  it('should not show examples if showExamples is false', () => {
+    // when
+    render(<ExampleDisplay wordId={wordId} examples={examples} showExamples={false} deleteExample={deleteExample}/>);
+
+    // then
+    expect(screen.queryByText(examples[0].example)).not.toBeInTheDocument();
+    expect(screen.queryByText(examples[1].example)).not.toBeInTheDocument();
+  });
+
+  it('should show a placeholder if no examples are provided', () => {
+    // when
+    render(<ExampleDisplay wordId={wordId} examples={[]} showExamples={true} deleteExample={deleteExample}/>);
+
+    // then
+    expect(screen.getByText("no examples")).toBeInTheDocument();
+  });
+
+  it('should delete an example', async () => {
+    // when
+    render(<ExampleDisplay wordId={wordId} examples={examples} showExamples={true} deleteExample={deleteExample}/>);
+
+    // then
+    const deleteExampleBtns = screen.getAllByLabelText("delete");
+
+    expect(deleteExampleBtns).toHaveLength(2);
+
+    // when
+    await userEvent.click(deleteExampleBtns[0]);
+
+    // then
+    expect(deleteExample).toHaveBeenCalledWith(wordId, 1);
+  });
+});
