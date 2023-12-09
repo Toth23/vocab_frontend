17db8689195e83b43d568b4b2073e74e6bcde937
Toth23 <thiagor@safe-mail.net>
2023-12-09 10:57:47 +0100

    improve greeting page (on small screens)

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,31 +1,36 @@
-import { Col, Empty, Row, Typography } from "antd";
+import { Empty, Typography } from "antd";
 import translatorIcon from "../assets/translator-icon.png";
 
 const { Paragraph, Title } = Typography;
 
 export const Greeting = () => (
-  <Row justify="center" align="middle" style={{ margin: 30 }}>
-    <Col span={8}>
-      <Empty
-        image={translatorIcon}
-        description={
-          <>
-            <Title level={3}>Hello there!</Title>
-            <Paragraph>
-              This is a simple website to record any vocabulary that you have
-              learned (optionally with translations and examples).
-            </Paragraph>
-            <Paragraph>
-              If you're new here, just look around and add some words that
-              you've recently learned.
-            </Paragraph>
-            <Paragraph>
-              If you've been here before and missing your data, you might want
-              to switch your User ID to what you had before.
-            </Paragraph>
-          </>
-        }
-      />
-    </Col>
-  </Row>
+  <div style={{ margin: 30 }}>
+    <Empty
+      image={translatorIcon}
+      description={
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Title level={3}>Hello there!</Title>
+          <Paragraph className={"greeting-paragraph"}>
+            This is a simple website to record any vocabulary that you have
+            learned (optionally with translations and examples).
+          </Paragraph>
+          <Paragraph className={"greeting-paragraph"}>
+            If you're new here, just look around and add some words that you've
+            recently learned.
+          </Paragraph>
+          <Paragraph className={"greeting-paragraph"}>
+            If you've been here before and missing your data, you might want to
+            switch your User ID to what you had before.
+          </Paragraph>
+        </div>
+      }
+    />
+  </div>
 );
